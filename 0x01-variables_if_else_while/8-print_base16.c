4f4c9cc0525f14b051398882d4c63f7defe1b57d
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-10 20:29:23 +0300

    Save 8-print_base16.c.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
  *  * main - Prints numbers between 0 to 9 and letters between a to f.
  *   *
@@ -6,16 +6,15 @@ nclude <stdio.h>
  *     */
 int main(void)
 {
-	        int i;
-		        for (i = 48; i < 58; i++)
-				        {
-						                putchar(i);
-								        }
-			        for (i = 97; i < 103; i++)
-					        {
-							                putchar(i);
-									        }
-				        putchar('\n');
-					        return (0);
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
 }
-
+for (i = 97; i < 103; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
