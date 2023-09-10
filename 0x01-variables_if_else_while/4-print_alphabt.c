d08668fd1ae7806dfd5feefc173aaffca8560b09
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-10 19:48:11 +0300

    Save 4-print_alphabt.c.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,10 @@ int main(void)
 
 for (i = 97; i < 123; i++)
 {
-    if (i != 101 && i != 113)
-    {
-        putchar(i);
-    }
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
 }
 putchar('\n');
 return (0);
