ad4132a330b7f4c8cebd5d4a880e1a7e4c335d37
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-10 19:33:19 +0300

    Save 3-print_alphabets.c.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,11 @@ include <stdio.h>
 int main(void)
 {
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int i;
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-		putchar('\n');
-	return (0);
+        int i;
+        for (i = 0; i < 52; i++)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
 }
