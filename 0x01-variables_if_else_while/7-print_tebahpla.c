9d491b8dbf16f478eeec0cf958acc0716f71b4f1
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-10 20:23:31 +0300

    Save 7-print_tebahpla.c.

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-        char ch;
-        for (ch = 'z'; ch >= 'a'; ch--)
-        {
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
