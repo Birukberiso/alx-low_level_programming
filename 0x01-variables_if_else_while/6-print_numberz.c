2b3bb8a66aa5bd4dc445bae9c66bb1a191375553
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-10 20:15:50 +0300

    Save 6-print_numberz.c.

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,11 @@ include <stdio.h>
  */
 int main(void)
 {
-int i;
-for (i = 48; i < 58; i++)
-{
-putchar(i);
-}
-putchar('\n');
-return (0);
+        int i;
+        for (i = 48; i < 58; i++)
+        {
+                putchar(i);
+        }
+        putchar('\n');
+        return (0);
 }
