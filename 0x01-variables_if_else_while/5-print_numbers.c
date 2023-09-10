59c67397f20cf391d1c57683efc2b3d00079ca1a
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-10 20:08:45 +0300

    Save 5-print_numbers.c.

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,16 @@
-include < stdio.h >
+#include <stdio.h>
 /**
-*  * main - Prints numbers from 0 to 9.
-*   *
-*    * Return: Always 0 (Success)
-*     */
-int main(void) {
-    int i;
-    for (i = 0; i < 10; i++) {
-        printf("%d", i);
-    }
-    putchar('\n');
-    return (0);
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+        for (i = 0; i < 10; i++)
+        {
+                printf("%d", i);
+        }
+        putchar('\n');
+        return (0);
 }
