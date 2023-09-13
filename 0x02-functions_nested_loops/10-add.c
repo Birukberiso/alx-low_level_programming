610694957f45fe57a2e0d9157a811d00c2f23906
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-13 22:43:02 +0300

    Save 10-add.c.

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,13 @@
-nclude "main.h"
+#include "main.h"
 /**
- *  * add - adds two integers and returns the result
- *   * @a: int to be added to b
- *    * @b: int to be added to a
- *     * Return: sum of a and b
- *      */
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
 int add(int a, int b)
 {
-	        int c;
-		        c = a + b;
-			        return (c);
+int c;
+c = a + b;
+return (c);
 }
-
