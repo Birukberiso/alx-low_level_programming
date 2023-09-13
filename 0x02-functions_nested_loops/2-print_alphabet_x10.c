920152567dd2fabf4b10b8a8264273323ebb8ba1
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-13 22:50:18 +0300

    Save 2-print_alphabet_x10.c.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ int i;
 i = 0;
 while (i < 10)
 {
-    ch = 'a';
-    while (ch <= 'z')
-    {
-        _putchar(ch);
-        ch++;
-    }
-    _putchar('\n');
-    i++;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
 }
 }
