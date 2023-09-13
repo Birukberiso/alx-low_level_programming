e612f43b1b892895e3632c64887df0cfb8cbc3df
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-13 23:07:00 +0300

    Save 100-times_table.c.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,6 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
@@ -15,7 +14,6 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 k = j * i;
-
 if (j == 0)
 {
 _putchar(k + '0');
