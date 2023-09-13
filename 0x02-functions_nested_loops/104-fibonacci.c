bc49639d57a535f2e430ebf088d2c71a82e2d826
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-13 23:28:41 +0300

    Save 104-fibonacci.c.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,15 +2,16 @@
 
 int main(void)
 {
-unsigned int fib1 = 1; // Initial Fibonacci number
-unsigned int fib2 = 2; // Second Fibonacci number
-printf("%u, %u", fib1, fib2); // Print the first two Fibonacci numbers
-for (int i = 2; i < 98; i++)
+unsigned int fib1 = 1; /* Initial Fibonacci number */
+unsigned int fib2 = 2; /* Second Fibonacci number */
+printf("%u, %u", fib1, fib2); /* Print the first two Fibonacci numbers */
+int i;
+for (i = 2; i < 98; i++)
 {
-unsigned int temp = fib1 + fib2; // Calculate the next Fibonacci number
-printf(", %u", temp); // Print the next Fibonacci number
-fib1 = fib2; // Update the first previous Fibonacci number
-fib2 = temp; // Update the second previous Fibonacci number
+unsigned int temp = fib1 + fib2; /* Calculate the next Fibonacci number */
+printf(", %u", temp); /* Print the next Fibonacci number */
+fib1 = fib2; /* Update the first previous Fibonacci number */
+fib2 = temp; /* Update the second previous Fibonacci number */
 }
 printf("\n");
 return 0;
