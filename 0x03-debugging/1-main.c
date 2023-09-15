a5ac674ea2398fdf55a99dae3eeff27263c97bda
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-16 01:20:34 +0300

    Save 1-main.c.

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
 /**
-* main - prints numbers from 0 to 9
+* main - causes an infinite loop
 * Return: 0
 */
 int main(void)
 {
 int i;
-printf("Printing numbers 0 to 9:\n");
+printf("Infinite loop incoming :(\n");
 i = 0;
-while (i < 10)
-{
-putchar('0' + i);
-i++;
-}
-printf("\nLoop completed!\n");
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
