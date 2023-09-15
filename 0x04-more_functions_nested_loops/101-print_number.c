b17f7e007df0df5bf3a785cc84d3ec11c5ddfaaf
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-16 00:04:41 +0300

    Save 101-print_number.c.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,16 +3,20 @@
 * print_number - prints an integer
 * @n: integer to be printed
 */
-void print_number(int n) {
+void print_number(int n)
+{
 unsigned int n1;
-if (n < 0) {
+if (n < 0)
+{
 n1 = -n;
 _putchar('-');
-} else
+}
+else
 {
 n1 = n;
 }
-if (n1 / 10) {
+if (n1 / 10)
+{
 print_number(n1 / 10);
 }
 _putchar((n1 % 10) + '0');
