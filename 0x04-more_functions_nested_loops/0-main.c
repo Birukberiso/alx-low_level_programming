dc38fc9794b4b666c5868ac268eff5546bcd7586
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-15 23:32:08 +0300

    Save 0-main.c.

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -6,10 +6,10 @@
 *    * Return: Always 0.
 *     */
 int main(void) {
-    char c;
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
 }
