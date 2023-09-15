39eb662e931d2e5ee8f7521e29e7b3c8127c7ce7
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-15 23:43:33 +0300

    Save 3-print_numbers.c.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,9 +4,10 @@
 * return: void
 */
 void print_numbers(void) {
-    char c;
-    for (c = '0'; c <= '9'; c++) {
-        _putchar(c);
-    }
-    _putchar('\n');
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
