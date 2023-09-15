b95aa3b15b1283928df94eb7a6c7c616ffd914d1
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-15 23:46:49 +0300

    Save 5-more_numbers.c.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,10 +2,12 @@
 /**
 * more_numbers - print more numbers
 */
-void more_numbers(void) {
+void more_numbers(void)
+{
 int i,
 j;
-for (i = 1; i <= 10; i++) {
+for (i = 1; i <= 10; i++)
+{
 for (j = 0; j <= 14; j++)
 {
 if (j >= 10)
