99033020a59bd57420fd3010998f6c550887c8fe
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-15 23:44:59 +0300

    Save 4-print_most_numbers.c.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,11 +4,13 @@
 *
 * return: void
 */
-void print_most_numbers(void) {
-    char c;
-    for (c = '0'; c <= '9'; c++) {
-        if (!(c == '2' || c == '4'))
-        _putchar(c);
-    }
-    _putchar('\n');
+void print_most_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+if (!(c == '2' || c == '4'))
+_putchar(c);
+}
+_putchar('\n');
 }
