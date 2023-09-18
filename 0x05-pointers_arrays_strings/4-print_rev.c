b3d4149cd8c86b762fba3357adba345a80a5cf43
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-18 18:29:54 +0300

    Save 4-print_rev.c.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,23 @@
-nclude "main.h"
+#include "main.h"
 /**
- *  * print_rev - imprime en reversa
- *   * @s: string
- *    * return: 0
- *     */
+* print_rev - imprime en reversa
+* @s: string
+* return: 0
+*/
 void print_rev(char *s)
 {
-	        int longi = 0;
-		        int o;
-			        while (*s != '\0')
-					        {
-							                longi++;
-									                s++;
-											        }
-				        s--;
-					        for (o = longi; o > 0; o--)
-							        {
-									                _putchar(*s);
-											                s--;
-													        }
-						        _putchar('\n');
+int longi = 0;
+int o;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+s--;
+for (o = longi; o > 0; o--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
 }
