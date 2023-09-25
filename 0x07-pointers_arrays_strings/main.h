5715fe7d1b5ded9ad99c99cc95e0793e7b6533f6
Birukberiso <birukberiso82@gmail.com>
2023-09-25 10:57:12 -0700

    updated

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+fndef MAIN_ H #define MAIN H
+char * memset (char *s, char b, unsigned int n); int _putchar(char c);
+char * memcpy(char "dest, char *src, unsigned int n);
+char * strchr(char 's, char c);
+unsigned int _strspn(char *s, char *accept);
+Char * strpbrk(char *5, char *accept);
+char * _strstr(char *haystack, char *needle);
+void print chessboard(char (*a)[8J);
+void print_diagsums(int *a, int size);
+void set_string(char **5, char *to);
+#endif
