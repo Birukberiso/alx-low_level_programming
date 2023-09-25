bc14e5351d33cea7584defa0130c6d274f20e5b3
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-25 21:04:55 +0300

    Save 2-strchr.c.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,17 @@
+#include "main.h"
+/**
+* _strchr - Entry point
+* @s: input
+* @c: input
+* Return: Always 0 (Success)
+*/
 char *_strchr(char *s, char c)
 {
-while (*s != '\0')
+int i = 0;
+for (; s[i] >= '\0'; i++)
 {
-if (*s == c)
-{
-return (s);
-}
-s++;
+if (s[i] == c)
+return (&s[i]);
 }
 return (0);
 }
