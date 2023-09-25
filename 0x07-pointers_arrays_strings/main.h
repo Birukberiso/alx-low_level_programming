76a9988d277c802cb1d682cd8d0b2e3dcaf0f575
Birukberiso <141414383+Birukberiso@users.noreply.github.com>
2023-09-25 21:00:43 +0300

    Save main.h.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,15 @@
 #ifndef MAIN_H
-#define MAIN H
-char * memset (char *s, char b, unsigned
-int n); int _putchar(char c);
-char * memcpy(char "dest, char *src, unsigned int n);
-char * strchr(char 's, char c);
+#define MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-Char * strpbrk(char *5, char *accept);
-char * _strstr(char *haystack, char *needle);
-void print chessboard(char (*a)[8J);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **5, char *to);
+void set_string(char **s, char *to);
+
 #endif
