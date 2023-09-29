6f6ffcd99d7a655f54a9d9227fc4e053d0e304af
Birukberiso <birukberiso82@gmail.com>
2023-09-29 13:57:34 -0700

    0x08-recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ *   * _print_rev_recursion - main function
+ *     *
+ *       * @s: Function parameter
+ *         *
+ *           * Return: Voidu
+ *             */
+void _print_rev_recursion(char *s)
+{
+	        if (*s)
+			        {
+					        _print_rev_recursion(s + 1);
+						        _putchar(*s);
+							        }
+}h
