56d78a1819237afc946868ce0fbd8ad16e3effa2
Birukberiso <birukberiso82@gmail.com>
2023-10-31 05:47:00 -0700

    0x15-file_io task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t read_count, write_count;
+	char *buffer;
+
+	if (filename == NULL)
+	return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	read_count = read(fd, buffer, letters);
+	if (read_count == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	write_count = write(STDOUT_FILENO, buffer, read_count);
+	if (write_count == -1 || write_count != read_count)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+
+	return (write_count);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+thei cskn
