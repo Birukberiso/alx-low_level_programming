8b2e8786b4cc7195b10e8dc532083197af4b0524
Birukberiso <birukberiso82@gmail.com>
2023-11-02 11:30:46 -0700

    0x15-file_io task advanced

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,58 +1,83 @@
+#include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <elf.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <string.h> /* for memcmp */
+#include <fcntl.h> /* for open */
+#include <unistd.h> /* for read, close */
+
+void check_elf(Elf64_Ehdr *header)
+{
+	if (memcmp(header->e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		fprintf(stderr, "Error: Not an ELF file\n");
+		exit(98);
+	}
+}
 
 void print_elf_header(Elf64_Ehdr *header)
 {
-	printf("Magic: ");
-	for (int i = 0; i < EI_NIDENT; i++)
-	printf("%02x ", header->e_ident[i]);
-	printf("\n");
-	printf("Class: %d\n", header->e_ident[EI_CLASS]);
-	printf("Data: %d\n", header->e_ident[EI_DATA]);
-	printf("Version: %d\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
-	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("Type: %d\n", header->e_type);
-	printf("Entry point address: 0x%lx\n", header->e_entry);
+	const char *class;
+	const char *data;
+	const char *os_abi;
+
+	printf("ELF Header:\n");
+	printf("  Magic: %02x %02x %02x %02x\n",
+		header->e_ident[0], header->e_ident[1],
+		header->e_ident[2], header->e_ident[3]);
+
+	class = header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64";
+	printf("  Class: %s\n", class);
+
+	data = header->e_ident[EI_DATA] == ELFDATA2LSB ?
+		"2's complement, little endian" :
+		"2's complement, big endian";
+	printf("  Data: %s\n", data);
+
+	os_abi = header->e_ident[EI_OSABI] == ELFOSABI_NONE ?
+		"UNIX - System V" :
+		(header->e_ident[EI_OSABI] == ELFOSABI_LINUX ?
+		"UNIX - Linux" : "unknown");
+	printf("  OS/ABI: %s\n", os_abi);
+
+	printf("  ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
+
+	printf("  Type: %s\n",
+		header->e_type == ET_EXEC ? "EXEC (Executable file)" :
+		(header->e_type == ET_DYN ? "DYN (Shared object file)" : "unknown"));
+
+	printf("  Entry point address: %lx\n", header->e_entry);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	int fd;
+	Elf64_Ehdr header;
+
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: elf_header elf_filename\n");
-		return (98);
+		fprintf(stderr, "Usage: %s <elf_file>\n", argv[0]);
+		exit(64);
 	}
 
-int fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 
 	if (fd == -1)
 	{
-		perror("Error opening file");
-		return (98);
+		perror("open");
+		exit(98);
 	}
 
-		Elf64_Ehdr header;
-	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	if (read(fd, &header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
 	{
-		perror("Error reading ELF header");
-		return (98);
+		perror("read");
+		exit(98);
 	}
 
-	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-		header.e_ident[EI_MAG1] != ELFMAG1 ||
-		header.e_ident[EI_MAG2] != ELFMAG2 ||
-		header.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		fprintf(stderr, "Not an ELF file\n");
-		return (98);
-	}
+	check_elf(&header);
 
 	print_elf_header(&header);
 
 	close(fd);
-	return (0);
+
+	return (EXIT_SUCCESS); /* use EXIT_SUCCESS instead of literal value */
 }
