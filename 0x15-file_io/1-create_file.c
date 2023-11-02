c73ab5b45ebcfc6538e0cd1af88b06cdb200056c
Birukberiso <birukberiso82@gmail.com>
2023-11-02 08:25:29 -0700

    0x1-file_io task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t write_count;
-	ssize_t length = 0;  /* Change the type of length to ssize_t */
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[length])
-			length++;
-		write_count = write(fd, text_content, length);
-	if (write_count == -1 || write_count != length)
-	{
-		close(fd);
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
-		}
-	}
 
 	close(fd);
+
 	return (1);
 }
